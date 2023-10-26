e943f035ca5532647768ddd1f326627534fb85bc
Ujjwal666 <66254824+Ujjwal666@users.noreply.github.com>
2023-10-26 11:20:48 -0500

    Add files via upload

---
+++ Code/Ujjwal/ChemistryBuildingActiveIPs.py
@@ -0,0 +1,71 @@
+import networkx as nx
+import matplotlib.pyplot as plt
+from scapy.all import traceroute, IP, UDP, sr1
+from random import randint
+import os
+
+def active_ips_from_file(fname, outname):
+
+    f = open(f"{outname}", "a")
+    
+    with open(fname, 'r') as file:
+
+        lines = file.readlines()
+        end = False
+        route = []
+
+        for line in lines:
+
+            if "hops away" in line and ":" in line:
+
+                ip = line.split(":")[1].strip()
+                parts = ip.split(".")
+
+                if parts[0] == "10":
+                    end = True
+                    print("Entered")
+                    route.append(ip)
+                    f.write(f"{ip}\n")
+                
+                if parts[0] == "138" and int(parts[1]) <= 238:
+                    end = True
+                    route.append(ip)
+                    f.write(f"{ip}\n")
+                    print("Entered")
+                
+            elif "STOP" in line:
+                print(f"Stopped at line {line}")
+                break
+            elif end == True:
+                f.write(f"Line End:\n")
+            else:
+                end = False
+                continue
+    f.close()
+
+
+if __name__ == "__main__":
+
+    active_ips_from_file("chemistry_building_10.199.txt", "active_ips_from_file.txt")
+
+    
+    routes = []
+    route = []
+
+    with open("active_ips_from_file.txt", 'r') as file:
+
+        lines = file.readlines()
+        
+        for line in lines:
+            if "Line End:" in line:
+                routes.append(route)
+                route = []
+            
+            else:
+                #print("Entered Append")
+                route.append(line)
+    
+    f = open(f"routes.txt", "a")
+    for i in routes:
+        f.write(f"{i}\n")
+    f.close()
